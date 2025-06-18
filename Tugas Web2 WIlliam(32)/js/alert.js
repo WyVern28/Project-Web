017b8de3a08f0c185ffe36b708b26e9ebe97419a
William <163488134+WyVern28@users.noreply.github.com>
2025-06-19 03:52:15 +0700

    Delete Tugas Web2 WIlliam(32) directory

--- Tugas Web2 WIlliam(32)/js/alert.js
+++
@@ -1,9 +0,0 @@
-let Email = document.querySelector('label');
-const Name = document.querySelector('label');
-
-const Submit = document.querySelector('button[type="button"]');
-Submit.addEventListener('click', function () {
-    if(Email.checked === false && Name.checked === false){
-        alert('INSERT YOUR EMAIL & YOUR NAME!!!!!');
-    } 
-})
